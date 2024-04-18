7773a4eb088560ea192efee6d21ebac11b4defda
SuperDev313 <pablo313boss@gmail.com>
2024-04-18 13:26:02 -0700

    Collection completed

---
+++ app/components/trending/trending.tsx
@@ -0,0 +1,15 @@
+import "./trending.css";
+
+type nftData = any[] | null;
+
+const Trending = ({
+  displayMode,
+  data,
+}: {
+  displayMode: string;
+  data?: nftData;
+}) => {
+  return <div></div>;
+};
+
+export default Trending;

499c9d26f1610d4ac794273d6cd99ef364b75f50
SuperDev313 <pablo313boss@gmail.com>
2024-04-18 06:21:37 -0700

    Collection File Init

---
+++ app/components/collection/collection.tsx
@@ -0,0 +1,7 @@
+import Trending from "./trending/trending";
+
+const Collection = ({ displayMode }: { displayMode: string }) => {
+  return <main className={collectionContainerClass}></main>;
+};
+
+export default Collection;

ead32ab57993165c57cb359a8f86fd463864a608
SuperDev313 <pablo313boss@gmail.com>
2024-04-24 21:02:14 -0700

    Profile page in Settings

--- app/components/settings/settings.tsx
+++ app/components/settings/settings.tsx
@@ -14,19 +14,31 @@ const Settings = ({ displayMode }: { displayMode: string }) => {
     updateProfileClass = "update-profile-lm";
     personalInfoClass = "personal--info-lm";
   }
-  return <main className={settingsContainer}>
-    <div className="page-header">
+  return (
+    <main className={settingsContainer}>
+      <div className="page-header">
         <h4>Settings</h4>
         <div className="lable--container">
-            <div>Welcome to your Settings</div>
-            <div id="label">
-                <div id="home"> Home</div>
-                <div>{`>`}</div>
-                <div id="collection"> Settings</div>
-            </div>
+          <div>Welcome to your Settings</div>
+          <div id="label">
+            <div id="home"> Home</div>
+            <div>{`>`}</div>
+            <div id="collection"> Settings</div>
+          </div>
         </div>
-    </div>
-  </main>;
+      </div>
+      <div className="settings" id="selector">
+        <div id="selected" className="selector--item">
+          Profile
+        </div>
+        <div className="selector--item">Application</div>
+        <div className="selector--item">Security</div>
+        <div className="selector--item">Activity</div>
+        <div className="selector--item">Payment Method</div>
+        <div className="selector--item">API</div>
+      </div>
+    </main>
+  );
 };
 
 export default Settings;

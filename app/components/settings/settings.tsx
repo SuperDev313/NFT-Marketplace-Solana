fde9c7cc4830e9bde2fa094cded8537b234bdb95
SuperDev313 <pablo313boss@gmail.com>
2024-04-24 21:01:44 -0700

    Header page in Settings

--- app/components/settings/settings.tsx
+++ app/components/settings/settings.tsx
@@ -1,8 +1,32 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 const Settings = ({ displayMode }: { displayMode: string }) => {
+  let settingsContainer, userInfoClass, updateProfileClass, personalInfoClass;
 
-    return <main></main>;
+  if (displayMode === "dark") {
+    settingsContainer = "settings--container";
+    userInfoClass = "user-info";
+    updateProfileClass = "update-profile";
+    personalInfoClass = "personal--info";
+  } else {
+    settingsContainer = "settings--container-lm";
+    userInfoClass = "user-info-lm";
+    updateProfileClass = "update-profile-lm";
+    personalInfoClass = "personal--info-lm";
+  }
+  return <main className={settingsContainer}>
+    <div className="page-header">
+        <h4>Settings</h4>
+        <div className="lable--container">
+            <div>Welcome to your Settings</div>
+            <div id="label">
+                <div id="home"> Home</div>
+                <div>{`>`}</div>
+                <div id="collection"> Settings</div>
+            </div>
+        </div>
+    </div>
+  </main>;
 };
 
 export default Settings;

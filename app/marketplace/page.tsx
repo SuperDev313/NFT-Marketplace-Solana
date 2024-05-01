9fb209de6ef7ea119ad27a69090fd55b0e78520e
SuperDev313 <pablo313boss@gmail.com>
2024-05-01 11:15:26 -0700

    import assets to marketplace page

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -1,4 +1,11 @@
 "use client";
+import Sidebar from "./components/sidebar/sidebar"
+import Header from "./components/header/header"
+import Dashboard from "./components/dashboard/dashboard"
+import Bid from "./components/bid/bid"
+import Collection from "./components/collection/collection"
+import Profile from "./components/profile/profile"
+import Settings from "./components/settings/settings"
 
 import { NextPage } from "next";
 
